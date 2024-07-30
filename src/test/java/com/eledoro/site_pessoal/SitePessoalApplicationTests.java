ba88215d63e429477713e5e51651d47c394f3512
julia-kw <knack.julia@escola.pr.gov.br>
2024-07-30 11:45:51 -0300

    arquivos novos

---
+++ src/test/java/com/eledoro/site_pessoal/SitePessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eledoro.site_pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
